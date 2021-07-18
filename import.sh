2bf115c4a334892e8456ee40579258df2b6294f6
Maximilinano Osorio <mosorio@isi.edu>
2021-07-18 14:46:40 -0400

    fix: import

--- import.sh
+++ import.sh
@@ -5,7 +5,7 @@ docker-compose exec endpoint \
 docker-compose exec endpoint \
     /jena/bin/tdbloader2   \
         --loc=/fuseki-base/databases/new_dataset \
-        /tmp/modelCatalog-18.0.nq.gz
+        /tmp/modelCatalog-1.8.0.nq.gz
 docker-compose exec endpoint \
     rm -rf /fuseki-base/databases/modelCatalog
 docker-compose exec endpoint \
