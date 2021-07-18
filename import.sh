172354efde56ac79f8dd4543207d3e9a33a4bcd9
Maximilinano Osorio <mosorio@isi.edu>
2021-07-18 14:44:22 -0400

    fix: upload 1.8.0

--- import.sh
+++ import.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 docker-compose up -d endpoint
 docker-compose exec endpoint \
-    wget   https://github.com/mintproject/fuseki-docker/blob/master/backups/modelCatalog-1.7.0_2021-05-20_14-02-34.nq.gz?raw=true -O /tmp/modelCatalog-1.7.0_2021-05-20_14-02-34.nq.gz
+    wget   https://github.com/mintproject/fuseki-docker/blob/master/backups/modelCatalog-1.8.0.nq.gz?raw=true -O /tmp/modelCatalog-1.8.0.nq.gz
 docker-compose exec endpoint \
     /jena/bin/tdbloader2   \
         --loc=/fuseki-base/databases/new_dataset \
-        /tmp/modelCatalog-1.7.0_2021-05-20_14-02-34.nq.gz
+        /tmp/modelCatalog-18.0.nq.gz
 docker-compose exec endpoint \
-    rm -rf /fuseki-base/databases/modelCatalog-1.7.0
+    rm -rf /fuseki-base/databases/modelCatalog
 docker-compose exec endpoint \
-    mv /fuseki-base/databases/new_dataset /fuseki-base/databases/modelCatalog-1.7.0
+    mv /fuseki-base/databases/new_dataset /fuseki-base/databases/modelCatalog
 docker-compose restart endpoint
