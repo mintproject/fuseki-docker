980caea886412183d08816171688969d61c10c3e
Maximilinano Osorio <mosorio@isi.edu>
2022-01-13 16:45:18 -0300

    fix: use tty

--- import.sh
+++ import.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 docker-compose up -d endpoint
-docker-compose exec endpoint \
+docker-compose exec -T  endpoint \
     wget   https://github.com/mintproject/fuseki-docker/blob/master/backups/modelCatalog-1.8.0.nq.gz?raw=true -O /tmp/modelCatalog-1.8.0.nq.gz
-docker-compose exec endpoint \
+docker-compose exec -T  endpoint \
     /jena/bin/tdbloader2   \
         --loc=/fuseki-base/databases/new_dataset \
         /tmp/modelCatalog-1.8.0.nq.gz
-docker-compose exec endpoint \
+docker-compose exec -T  endpoint \
     rm -rf /fuseki-base/databases/modelCatalog
-docker-compose exec endpoint \
+docker-compose exec -T  endpoint \
     mv /fuseki-base/databases/new_dataset /fuseki-base/databases/modelCatalog
 docker-compose restart endpoint
